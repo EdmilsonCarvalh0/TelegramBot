826578d3ef5c0253a063f12e7c74738cb728a4dc
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-03-27 14:59:50 -0300

    Implementa métodos que lidam com remoção e alteração de itens.

--- Application/Handlers/MessageHandler.cs
+++ Application/Handlers/MessageHandler.cs
@@ -19,8 +19,6 @@ public class MessageHandler
 
         ProcessMenuRequest();
 
-        if (_responseInfo == null) return _responseInfo!;
-
         var userStateData = _handlerContext.StateManager.GetUserStateData(_handlerContext.Context!.UserId);
 
         switch (userStateData.AdditionalInfo)
@@ -35,6 +33,14 @@ public class MessageHandler
 
             case "waiting_for_name_attribute_to_update":
                 HandleWaitingForNameAttributeToUpdated();
+                break;
+
+            case "waiting_for_number_that_references_to_update":
+                HandleNumberReferencingItem();
+                break;
+
+            case "waiting_for_number_that_references_to_remove":
+                HandleNumberReferencingItem();
                 break;
                 
             case "waiting_item_to_remove":
@@ -75,12 +81,39 @@ public class MessageHandler
 
     private void HandleWaitingForAttributeToUpdated()
     {
+        var attribute = _handlerContext.Context!.Message!.Text!;
+        _handlerContext.ItemRepository.UpdateItemInList(attribute);
+        _handlerContext.StateManager.ResetAdditionalInfo(_handlerContext.Context!.UserId);
 
+        _responseInfo.Subject = "Update Item OK";
     }
 
     private void HandleWaitingForNameAttributeToUpdated()
     {
+        var nameAttribute = _handlerContext.Context!.Message!.Text!;
+        var searchResult = _handlerContext.ItemRepository.GetItemInRepository(nameAttribute!);
 
+        if (searchResult.Status == SearchStatus.NotFound)
+        {
+            _responseInfo.Subject = "Non-existent Item";
+            _responseInfo.SubjectContextData = _handlerContext.ItemRepository.GetList();
+            return;
+        }
+
+        if (searchResult.Status == SearchStatus.MoreThanOne)
+        {
+            _responseInfo.Subject = "More Than One Item";
+            _responseInfo.SubjectContextData = "";
+            _responseInfo.SubjectContextData = searchResult.Result;
+            _handlerContext.StateManager.SetAdditionalInfo(_handlerContext.Context!.UserId, "waiting_for_number_that_references_to_update");
+            return;
+        }
+
+        var genderVerified = CheckAttributeGender(nameAttribute);
+        _responseInfo.SubjectContextData = $"{genderVerified} {nameAttribute}";
+
+        _handlerContext.ItemRepository.AddItemInEditingArea(nameAttribute!);
+        _responseInfo.Subject = "Update Item";
     }
 
     private void HandleWaitingItemToRemove()
@@ -97,15 +130,67 @@ public class MessageHandler
         if (result.Status == SearchStatus.MoreThanOne)
         {
             _responseInfo.Subject = "More Than One Item";
+            _responseInfo.SubjectContextData = "";
             _responseInfo.SubjectContextData = result.Result;
+            _handlerContext.StateManager.SetAdditionalInfo(_handlerContext.Context!.UserId, "waiting_for_number_that_references_to_remove");
             return;
         }
 
+        _handlerContext.StateManager.ResetAdditionalInfo(_handlerContext.Context.UserId);
         _responseInfo.Subject = "Deleted Item OK";
+    }
+
+    private void HandleNumberReferencingItem()
+    {
+        var userId = _handlerContext.Context!.UserId;
+        var stateData = _handlerContext.StateManager.GetUserStateData(userId);
+        var inputNumber = _handlerContext.Context!.Message!.Text!;
+
+        bool isNumber = CheckIfItIsANumber(inputNumber);
+        int referenceNumber = 0;
+
+        if(isNumber)
+        {
+            referenceNumber = Convert.ToInt32(inputNumber);
+        } 
+        
+        if(!isNumber)
+        {
+            _responseInfo.Subject = "Invalid Number";
+            return;
+        }
+
+        UserState response = _handlerContext.ItemRepository.VerifyNumberReferencingItem(referenceNumber, stateData.AdditionalInfo);
+
+        switch (response)
+        {
+            case UserState.None:
+                _responseInfo.Subject = "Invalid Number";
+                break;
+
+            case UserState.UpdateItem:
+                _responseInfo.Subject = "Update Item";
+                break;
+
+            case UserState.DeleteItem:
+                _responseInfo.Subject = "Deleted Item OK";
+                _handlerContext.StateManager.ResetAdditionalInfo(_handlerContext.Context!.UserId);
+                break;
+        }
     }
 
     private void HandleWaitingItemsCreatingNewList()
     {
 
+    }
+
+    private string CheckAttributeGender(string item)
+    {
+        return item[item.Length - 1] == 'a' ? "da " : "do ";
+    }
+
+    private bool CheckIfItIsANumber(string inputNumber)
+    {
+        return inputNumber.Length == 1 && int.TryParse(inputNumber, out _);
     }
 }
