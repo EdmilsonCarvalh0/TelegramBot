6b1557fcb59796d29d896fa8528fcd2f05c36338
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-03-30 10:18:47 -0300

    Refatora classe para garantir responsabilidade única.

--- UserInterface/IResponseManager.cs
+++ UserInterface/IResponseManager.cs
@@ -2,17 +2,5 @@ using TelegramBot.Application;
 
 public interface IResponseManager
 {
-    ResponseContentDTO GetResponseCallback(string subject);
-    ResponseContentDTO GetResponseMessage(string subject);
-    ResponseContentDTO GetInitialMessage(string subject);
-    ResponseContentDTO StartService(string request);
-    ResponseContentDTO GetOptionsOfListUpdate(string request);
-    ResponseContentDTO GetAttributeOptions(string request);
-    ResponseContentDTO CheckItemExistence(string nameAttribute);
-    ResponseContentDTO AddItemInShoppingData(string item);
-    ResponseContentDTO SendAttributeToUpdateItem(string item);
-    void SendAttributeToEditingArea(string attributeToBeChagend);
-    ResponseContentDTO SendItemToRemoveFromList(string item);
-    ResponseContentDTO ShowList();
-    ResponseContentDTO GetItemsToCreatelist(string item);
+    Task ProcessResponse(ResponseInfoToSendToTheUser reponseInfo, CancellationToken cancellationToken);
 }
