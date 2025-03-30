85743d2d9087918a6e8fc6e7720eefc4fd53f8a3
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-03-30 10:32:13 -0300

    Renomeia e implementa novos status de busca para AssistantMode.

--- Domain/Enums/UserState.cs
+++ Domain/Enums/UserState.cs
@@ -3,12 +3,14 @@ namespace TelegramBot.Domain;
 public enum UserState
 {
     None,
-    ServicePaused,
     Running,
     GetList,
     UpdateList,
     AddItem,
     UpdateItem,
     DeleteItem,
-    CreateList
+    CreateList,
+    ConfirmShoppingMode,
+    PreparingShoppingMode,
+    OnShoppingMode
 }
