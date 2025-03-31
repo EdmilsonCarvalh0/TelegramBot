a6b3b3f63086f9fac06d5a1baf21a14d41c688fb
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-03-31 15:49:10 -0300

    Refatoração: Implementado HostBuilder para gerenciar configuração e serviços do bot. Adicionada injeção de dependências.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿using Telegram.Bot;
-using Telegram.Bot.Polling;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
+using TelegramBot.Application;
+using TelegramBot.Data;
 using TelegramBot.Infrastructure;
+using TelegramBot.Service;
 using TelegramBot.UserInterface;
 
 namespace TelegramBot
@@ -11,28 +15,73 @@ namespace TelegramBot
     {
         static async Task Main(string[] args)
         {
-            BotConnection client = new BotConnection();
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                    {
+                        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    }
+                )
+                .ConfigureServices((context, services) =>
+                    {
+                        services.AddSingleton<ITelegramBotClient>(provider =>
+                        {
+                            var configuration = provider.GetRequiredService<IConfiguration>();
+                            var botToken = configuration["BotSettings:Token"]
+                                            ?? throw new InvalidOperationException("Token do bot não foi encontrado.");
+                            
+                            return new TelegramBotClient(botToken);
+                        });
+                        services.AddSingleton<ChatIdIdentifier>(provider =>
+                        {
+                            var configuration = provider.GetRequiredService<IConfiguration>();
+                            var userId = configuration["BotSettings:UserId"]
+                                        ?? throw new InvalidOperationException("UserId não encontrado.");
+                            
+                            return new ChatIdIdentifier(long.Parse(userId));
+                        });
+                        services.AddSingleton<SearchResultHandler>();
+                        services.AddSingleton<IItemRepository, JsonItemRepository>(provider =>
+                        {
+                            var configuration = provider.GetRequiredService<IConfiguration>();
+                            var searchResultHandler = provider.GetRequiredService<SearchResultHandler>();
+                            var serviceProvider = provider.GetRequiredService<IServiceProvider>();
 
-            using var cts = new CancellationTokenSource();
+                            var filePath = configuration["FilePaths:ItemsRepository"] 
+                                        ?? throw new InvalidOperationException("Caminho de arquivo para repositório de itens não encontrado.");
+                                        
+                            return new JsonItemRepository(searchResultHandler, serviceProvider, filePath);
+                        });
+                        services.AddSingleton<BotResponse>(provider =>
+                        {
+                            var configuration = provider.GetRequiredService<IConfiguration>();
+                            var filePath = configuration["FilePaths:ResponsesRepository"]
+                                        ?? throw new InvalidOperationException("Caminho de arquivo para repositório de informações de resposta não encontrado.");
+                            
+                            return new BotResponse(filePath);
+                        });
+                        services.AddSingleton<ShoppingAssistantMode>();
+                        services.AddSingleton<BotConnection>();
+                        services.AddSingleton<UpdateHandlers>();
+                        services.AddSingleton<UserStateManager>();
+                        services.AddSingleton<IResponseManager, ResponseManager>();
+                        services.AddSingleton<MessageSender>();
+                        services.AddSingleton<ItemDataFormatter>();
+                        services.AddSingleton<CallbackQuery>();
+                        services.AddSingleton<Message>();
+                        services.AddSingleton<HandlerContext>();
+                        services.AddScoped<IEditingArea, EditingArea>();
+                        services.AddScoped<BotRequestContextFactory>();
+                    }
+                );
 
-            // Receiver configurations
-            client.Bot.StartReceiving(
-                BotConnection.HandleUpdateAsync,
-                BotConnection.HandleErrorAsync,
-                new ReceiverOptions
-                {
-                    AllowedUpdates = Array.Empty<UpdateType>()
-                },
-                cancellationToken: cts.Token
-            );
+            var app = builder.Build();
 
-            //Exibe informações do bot
-            var me = await client.Bot.GetMe();
-            Console.WriteLine($"Bot iniciado: @{me.Username}");
-            Console.ReadLine();
+            var bot = app.Services.GetRequiredService<BotConnection>();
 
-            //Encerra o bot
-            cts.Cancel();
+            await bot.DisplayBotInfoAsync();
+            bot.Start();
+
+            await app.RunAsync();
         }
     }
 }
