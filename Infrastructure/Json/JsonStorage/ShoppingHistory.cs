9e8b38d9e668f093ad849060051c2368c68351d7
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-04-27 19:54:07 -0300

    feat(shoppingHistory): permite atualizar um snapshot existente no histórico de compras

--- Infrastructure/Json/JsonStorage/ShoppingHistory.cs
+++ Infrastructure/Json/JsonStorage/ShoppingHistory.cs
@@ -20,10 +20,25 @@ public class ShoppingHistory
 
     public void SavePurchasedItems(ShoppingListSnapshot snapshot)
     {
-        var snapshots = _jsonFileReader.ReadFromFile<List<ShoppingListSnapshot>>(FileType.Items);
+        var snapshots = GetSnapShots();
         
         snapshots.Add(snapshot);
         
         _jsonFileReader.WriteToFile(FileType.Items, snapshots);
+    }
+
+    public void SavingChangedSnapshot(ShoppingListSnapshot snapshotToSave)
+    {
+        var snapshots = GetSnapShots();
+        
+        var index = snapshots.FindIndex(s=> s.Equals(snapshotToSave));
+        snapshots[index] = snapshotToSave;
+        
+        _jsonFileReader.WriteToFile(FileType.Items, snapshots);
+    }
+
+    private List<ShoppingListSnapshot> GetSnapShots()
+    {
+        return _jsonFileReader.ReadFromFile<List<ShoppingListSnapshot>>(FileType.Items);
     }
 }
